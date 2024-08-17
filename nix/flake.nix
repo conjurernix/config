69e60028310f510c886409541bd54d26a71b1a17
DrBuchkov <pafitisnick@gmail.com>
2024-08-17 03:48:03 +0300

    Changes

--- nix/flake.nix
+++ nix/flake.nix
@@ -35,7 +35,12 @@
       system.stateVersion = 4;
 
       # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "x86_64-darwin";
+      nixpkgs.hostPlatform = "aarch64-darwin";
+
+      users.users.nikolaspafitis = {
+       name = "nikolaspafitis";
+       home = "/Users/nikolaspafitis";
+      };
     };
   in
   {
