5745e25f95f1cc3ab3bf7e594da7afbac68d10f2
DrBuchkov <pafitisnick@gmail.com>
2025-02-05 23:36:20 +0200

    Removing ghostty package from flake

--- nix/flake.nix
+++ nix/flake.nix
@@ -58,22 +58,22 @@
             btop
             bat
             eza
-	          tree
-	          cloc
-	          fontconfig
-	          fd
-	          gnugrep
-	          ## ZSH packages
-	          zsh
-	          oh-my-zsh
-	          #### neovim packages
+	    tree
+	    cloc
+	    fontconfig
+	    fd
+	    gnugrep
+	    ## ZSH packages
+	    zsh
+	    oh-my-zsh
+	    #### neovim packages
             neovim
-	          lunarvim
-	          ####
-		  emacs
-	          #### Fonts
-	          fira
-	          ######
+	    lunarvim
+	    ####
+	    emacs
+	    #### Fonts
+	    fira
+	    ######
             lazygit
             pandoc
             neofetch
@@ -85,11 +85,11 @@
             vpsfree-client
             # Window Manager and Desktop
             skhd
-	          sketchybar
-	          ######
+	    sketchybar
+	    ######
             kitty
-	          ###
-	          cocoapods
+	    ###
+	    cocoapods
             # C
 	    ninja
 	    meson
